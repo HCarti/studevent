a6d8e3c3553596e2b602bcd80b1c7b8db074e71a
CoderCarti <bladeruneking@gmail.com>
2024-11-07 10:49:34 +0800

    jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,7 +9,7 @@
     try {
       const user = await User.findOne({ email: email });
       console.log(user)
-      if (!user) {
+      if (!user) {  
         return res.status(401).json({ message: 'Invalid email or password.' });
       }
 
@@ -20,15 +20,7 @@
 
       }
 
-      // const isMatch = await bcrypt.compare(password, user.password);
-      // if (!isMatch) {
-      //   return res.status(401).json({ message: 'Invalid email or password.' });
-      // }
-
-      // const token = jwt.sign({ userId: user._id, role: user.role }, 'your_jwt_secret', { expiresIn: '1h' });
-
-      // res.status(200).json({ token, user: { email: user.email, role: user.role, firstName: user.firstName, lastName: user.lastName } });
-      // res.status(200).json(user)
+    
     } catch (error) {
       res.status(500).json({ message: 'Error logging in' });
     }
