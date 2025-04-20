687ac2998af6f8616335fdb81395881234dd561b
CoderCarti <bladeruneking@gmail.com>
2025-04-20 18:50:30 +0800

    form undefined fix

--- server/routes/formRoutes.js
+++ server/routes/formRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const formController = require('../controllers/formController');
+const Form = require('../models/Form');
 
 // Configure multer for file uploads
 const upload = multer({
